78a6694b8f261071822db3ed15893b57ad3f1881
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-17 12:47:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,22 @@
 def nyc_pigeon_organizer(data)
 
+  pigeons = {}
+
   data.each_pair do |attributes, inner_hash|
 
 
     inner_hash.each_pair do |inner_attribute, names|
-      p inner_attribute
-      p names
+
+      names.each { |name| 
+
+        if !pigeons[name]
+          pigeons[name] = {}
+        end
+        
+       }
 
 
     end
   end
-
+ pigeons
 end
