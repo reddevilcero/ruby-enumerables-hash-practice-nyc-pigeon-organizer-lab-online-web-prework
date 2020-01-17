afc7c2ed4e956a6053eb040ffad891be50804539
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-17 13:15:44 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  pigeons = {}
+
+  data.each_pair do |attributes, inner_hash|
+
+    inner_hash.each_pair do |inner_attribute, names|
+      names.each { |name|
+
+        if !pigeons[name]
+          pigeons[name] = {}
+        end
+
+        if data[attributes][inner_attribute].include?(name)
+          if !pigeons[name][attributes]
+              pigeons[name][attributes] = []
+          end
+          pigeons[name][attributes].push(inner_attribute.to_s)
+        end
+       }
+    end
+  end
+ pigeons
 end
