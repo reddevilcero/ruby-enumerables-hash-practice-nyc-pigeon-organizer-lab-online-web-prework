e8a11fbb883222b875f9bc4c221c3859a5c73548
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-17 12:42:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,10 @@
 def nyc_pigeon_organizer(data)
 
-  data.each_pair { |key, value|
-    p key
-    p value
+  data.each_pair { |attributes, inner_hash|
+
+    inner_hash.each_pair { |inner_attribute, names|  }
+            p inner_attribute
+            p names
     }
 
 end
